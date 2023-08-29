e5291e56a990b755fc0091bc07b3eaf452f2a370
Kelly Caylor <caylor@ucsb.edu>
2023-08-28 20:12:37 -0700

    reorganized repo

---
+++ lectures/obfuscation.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+# What does this code do??
+# It looks impossibly complex...
+@P=split//,".URRUUxR";@d=split//,"\nlanruoJ lreP ehT";
+sub p{@p{"r$p","u$p"}=(P,P);pipe"r$p","u$p";$p++;($q*=
+2)+=$f=!fork;map{$P=$P[$f|6&ord$p{$_}];$p{$_}=/$P/i?$P:
+close$_}%p}p;p;p;p;p;map$p{$_}=~/[P.]/&&close$_,%p;wait
+until$?;map/r/&&<$_>,%p;print$d[$q]
--- resources/hello_world.py
+++
@@ -1,3 +0,0 @@
-
-print("Hello world!")
-print("[from hello_world.py]")
